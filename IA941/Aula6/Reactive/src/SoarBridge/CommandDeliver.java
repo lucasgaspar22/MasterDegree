c94d70677002497a2df06bcdf8bca84ea0701a5f
Lucas Gaspar <lucasprgaspar@gmail.com>
2020-05-16 18:51:21 -0300

    Entrega 2
    Reativa

---
+++ IA941/Aula6/Reactive/src/SoarBridge/CommandDeliver.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SoarBridge;
+
+/**
+ *
+ * @author lucas
+ */
+public class CommandDeliver {
+    
+    private String thingName = null;
+
+    public CommandDeliver(){}
+
+    /**
+     * @return the thingName
+     */
+    public String getThingName()
+    {
+        return thingName;
+    }
+
+    /**
+     * @param thingName the thingName to set
+     */
+    public void setThingName(String thingName)
+    {
+        this.thingName = thingName;
+    }
+}

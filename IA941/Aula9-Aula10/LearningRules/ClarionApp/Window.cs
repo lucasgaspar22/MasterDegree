7d8d2246f02f9d2e327e6b341d1f730a8ea7572d
Lucas Gaspar <lucasprgaspar@gmail.com>
2020-06-21 18:03:56 -0300

    Use a Neural Network to train the agent

---
+++ IA941/Aula9-Aula10/LearningRules/ClarionApp/Window.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ClarionDEMO {
+    public partial class Window : Gtk.Window {
+        public Window ():
+            base (Gtk.WindowType.Toplevel) {
+                this.Build ();
+            }
+    }
+}

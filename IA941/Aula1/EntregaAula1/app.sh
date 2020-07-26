7a75c2a2b03b5bcbaa9dc3d3ba22ed69fe0e25a5
Lucas Gaspar <lucasprgaspar@gmail.com>
2020-07-26 14:15:12 -0300

    reset master branch

--- IA941/Aula1/EntregaAula1/app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nohup java -jar ws3d-all-0.0.1.jar &
-sleep 5
-java -jar EntregaAula1-all-0.0.1.jar  
--- IA941/Aula9-Aula10/LearningRules/index.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nohup java -jar ws3d-all-0.0.1.jar &
-sleep 8
-mono ./ClarionApp/bin/Release/ClarionApp.exe
